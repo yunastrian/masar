e034f998d2d6a66070c1dd69b91326014449beab
Kurniandha Sukma Yunastrian <13516106@std.stei.itb.ac.id>
2020-02-10 00:57:06 +0700

    add HomeScreen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,198 +1,135 @@
-import * as WebBrowser from 'expo-web-browser';
 import React from 'react';
-import {
-  Image,
-  Platform,
-  ScrollView,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
-} from 'react-native';
+import { FlatList, ScrollView, StyleSheet, View, ListView, Text, Image } from 'react-native';
+import NewItemBox from '../components/NewItemBox';
+import Promotions from '../components/Promotions';
 
-import { MonoText } from '../components/StyledText';
+const results = [
+  {
+    itemImage: 'Untitled.png',
+    productName: 'Tas mustardo buatan tangan',
+    productPriceAfter: '23000',
+  },
+  {
+    itemImage: 'Untitled.png',
+    productName: 'Tas mustard dengan pegangan',
+    productPriceAfter: '223000',
+  },
+  {
+    itemImage: 'Untitled.png',
+    productName: 'Tas mustard dengan pegangan',
+    productPriceAfter: '223000',
+  },
+  {
+    itemImage: 'Untitled.png',
+    productName: 'Tas mustard dengan pegangan',
+    productPriceAfter: '223000',
+  },
+  {
+    itemImage: 'Untitled.png',
+    productName: 'Tas mustard dengan pegangan',
+    productPriceAfter: '223000',
+  },
+  {
+    itemImage: 'Untitled.png',
+    productName: 'Tas mustard dengan pegangan',
+    productPriceAfter: '223000',
+  },
+  {
+    itemImage: 'Untitled.png',
+    productName: 'Tas mustard dengan pegangan',
+    productPriceAfter: '223000',
+  }
+]
 
-export default function HomeScreen() {
-  return (
-    <View style={styles.container}>
+export default class HomeScreen extends React.Component {
+  render() {
+    return (
+      <ScrollView style={styles.container}>
       <ScrollView
-        style={styles.container}
-        contentContainerStyle={styles.contentContainer}>
-        <View style={styles.welcomeContainer}>
-          <Image
-            source={
-              __DEV__
-                ? require('../assets/images/robot-dev.png')
-                : require('../assets/images/robot-prod.png')
-            }
-            style={styles.welcomeImage}
-          />
-        </View>
-
-        <View style={styles.getStartedContainer}>
-          <DevelopmentModeNotice />
-
-          <Text style={styles.getStartedText}>Get started by opening</Text>
-
-          <View
-            style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
-            <MonoText>screens/HomeScreen.js</MonoText>
-          </View>
-
-          <Text style={styles.getStartedText}>
-            Change this text and your app will automatically reload.
-          </Text>
-        </View>
-
-        <View style={styles.helpContainer}>
-          <TouchableOpacity onPress={handleHelpPress} style={styles.helpLink}>
-            <Text style={styles.helpLinkText}>
-              Help, it didn’t automatically reload!
-            </Text>
-          </TouchableOpacity>
-        </View>
+        horizontal
+        pagingEnabled
+        showsHorizontalScrollIndicator={false}
+        style={styles.promo}
+      >
+        <Promotions content='Promo Gajian Diskon Hingga 90%' colour='#d45656'/>
+        <Promotions content='Promo Merdeka Diskon Hingga 45%' colour='#3c4d85'/>
       </ScrollView>
-
-      <View style={styles.tabBarInfoContainer}>
-        <Text style={styles.tabBarInfoText}>
-          This is a tab bar. You can edit it in:
-        </Text>
-
-        <View
-          style={[styles.codeHighlightContainer, styles.navigationFilename]}>
-          <MonoText style={styles.codeHighlightText}>
-            navigation/MainTabNavigator.js
-          </MonoText>
-        </View>
-      </View>
-    </View>
-  );
-}
-
-HomeScreen.navigationOptions = {
-  header: null,
-};
-
-function DevelopmentModeNotice() {
-  if (__DEV__) {
-    const learnMoreButton = (
-      <Text onPress={handleLearnMorePress} style={styles.helpLinkText}>
-        Learn more
-      </Text>
-    );
-
-    return (
-      <Text style={styles.developmentModeText}>
-        Development mode is enabled: your app will be slower but you can use
-        useful development tools. {learnMoreButton}
-      </Text>
-    );
-  } else {
-    return (
-      <Text style={styles.developmentModeText}>
-        You are not in development mode: your app will run at full speed.
-      </Text>
+      <Text style={styles.more}>Lihat semua</Text>
+      <Image
+        style={{margin: 16.5,
+                resizeMode: 'contain'}}
+        source={require('../coba.png')}
+      />
+      <Text style={styles.newArr}>Barang Baru</Text>
+      <Text style={styles.more}>Lihat semua</Text>
+      <ScrollView
+        horizontal
+        showsHorizontalScrollIndicator={false}
+      >
+        <NewItemBox 
+          productName = {results[0].productName}
+          productPriceAfter = {results[0].productPriceAfter}
+        />
+        <NewItemBox 
+          productName = {results[0].productName}
+          productPriceAfter = {results[0].productPriceAfter}
+        />
+        <NewItemBox 
+          productName = {results[0].productName}
+          productPriceAfter = {results[0].productPriceAfter}
+        />
+        <NewItemBox 
+          productName = {results[0].productName}
+          productPriceAfter = {results[0].productPriceAfter}
+        />
+      </ScrollView>
+     
+      <Text style={styles.newArr}>Pencarian Populer</Text>
+      <Text style={styles.more}>Lihat semua</Text>
+      <ScrollView
+        horizontal
+        showsHorizontalScrollIndicator={false}
+      >
+        <NewItemBox 
+          productName = {results[0].productName}
+          productPriceAfter = {results[0].productPriceAfter}
+        />
+        <NewItemBox 
+          productName = {results[0].productName}
+          productPriceAfter = {results[0].productPriceAfter}
+        />
+        <NewItemBox 
+          productName = {results[0].productName}
+          productPriceAfter = {results[0].productPriceAfter}
+        />
+        <NewItemBox 
+          productName = {results[0].productName}
+          productPriceAfter = {results[0].productPriceAfter}
+        />
+      </ScrollView>
+    </ScrollView>
     );
   }
-}
-
-function handleLearnMorePress() {
-  WebBrowser.openBrowserAsync(
-    'https://docs.expo.io/versions/latest/workflow/development-mode/'
-  );
-}
-
-function handleHelpPress() {
-  WebBrowser.openBrowserAsync(
-    'https://docs.expo.io/versions/latest/workflow/up-and-running/#cant-see-your-changes'
-  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    paddingTop: 4,
+    backgroundColor: 'rgb(235,235,235)'
   },
-  developmentModeText: {
-    marginBottom: 20,
-    color: 'rgba(0,0,0,0.4)',
-    fontSize: 14,
-    lineHeight: 19,
-    textAlign: 'center',
+  newArr: {
+    fontWeight: 'bold',
+    fontSize: 18,
+    marginLeft: 4,
+    paddingTop: 4,
+    marginBottom: -20
   },
-  contentContainer: {
-    paddingTop: 30,
+  more: {
+    textAlign: 'right',
+    paddingRight: 4
   },
-  welcomeContainer: {
-    alignItems: 'center',
-    marginTop: 10,
-    marginBottom: 20,
-  },
-  welcomeImage: {
-    width: 100,
-    height: 80,
-    resizeMode: 'contain',
-    marginTop: 3,
-    marginLeft: -10,
-  },
-  getStartedContainer: {
-    alignItems: 'center',
-    marginHorizontal: 50,
-  },
-  homeScreenFilename: {
-    marginVertical: 7,
-  },
-  codeHighlightText: {
-    color: 'rgba(96,100,109, 0.8)',
-  },
-  codeHighlightContainer: {
-    backgroundColor: 'rgba(0,0,0,0.05)',
-    borderRadius: 3,
-    paddingHorizontal: 4,
-  },
-  getStartedText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    lineHeight: 24,
-    textAlign: 'center',
-  },
-  tabBarInfoContainer: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    ...Platform.select({
-      ios: {
-        shadowColor: 'black',
-        shadowOffset: { width: 0, height: -3 },
-        shadowOpacity: 0.1,
-        shadowRadius: 3,
-      },
-      android: {
-        elevation: 20,
-      },
-    }),
-    alignItems: 'center',
-    backgroundColor: '#fbfbfb',
-    paddingVertical: 20,
-  },
-  tabBarInfoText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    textAlign: 'center',
-  },
-  navigationFilename: {
-    marginTop: 5,
-  },
-  helpContainer: {
-    marginTop: 15,
-    alignItems: 'center',
-  },
-  helpLink: {
-    paddingVertical: 15,
-  },
-  helpLinkText: {
-    fontSize: 14,
-    color: '#2e78b7',
-  },
+  promo: {
+    margin: 5
+  }
 });
